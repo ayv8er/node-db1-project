605d8ca349b76cf13b141eaea9377d987628cab2
Richard Oh <n24512@protonmail.com>
2021-10-04 15:10:06 -0500

    getAll and getById working, checkAccountId middleware functional

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,11 +1,24 @@
+const Accounts = require("./accounts-model");
+
 exports.checkAccountPayload = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+  next();
+};
 
 exports.checkAccountNameUnique = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+  next();
+};
 
 exports.checkAccountId = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+  Accounts.getById(req.params.id)
+    .then((account) => {
+      if (!account) {
+        res.status(404).json({
+          message: "Account ID does not exists",
+        });
+      } else {
+        req.acc = account;
+        next();
+      }
+    })
+    .catch(next);
+};
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,37 @@
-const router = require('express').Router()
+const router = require("express").Router();
+const { checkAccountId } = require("./accounts-middleware");
+const Accounts = require("./accounts-model");
 
-router.get('/', (req, res, next) => {
-  // DO YOUR MAGIC
-})
+router.get("/", (req, res, next) => {
+  Accounts.getAll()
+    .then((accounts) => {
+      res.status(200).json(accounts);
+    })
+    .catch(next);
+});
 
-router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
-})
+router.get("/:id", checkAccountId, (req, res, next) => {
+  res.status(200).json(req.acc);
+});
 
-router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
-})
-
-router.put('/:id', (req, res, next) => {
+router.post("/", (req, res, next) => {
   // DO YOUR MAGIC
 });
 
-router.delete('/:id', (req, res, next) => {
+router.put("/:id", (req, res, next) => {
   // DO YOUR MAGIC
-})
+});
 
-router.use((err, req, res, next) => { // eslint-disable-line
+router.delete("/:id", (req, res, next) => {
   // DO YOUR MAGIC
-})
+});
+
+router.use((err, req, res, next) => {
+  // eslint-disable-line
+  res.status(err.status || 500).json({
+    message: err.message,
+    stack: err.stack,
+  });
+});
 
 module.exports = router;
